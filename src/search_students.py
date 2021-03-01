e4a87c72671d255571c611a1b64d83e3ea1585f6
lokajayae <lokajaya414@gmail.com>
2021-03-01 11:36:43 +0700

    Implementing search class by student id and opencv camera capture

---
+++ src/search_students.py
@@ -0,0 +1,36 @@
+import constants as dotenv
+import pandas as pd
+from pathlib import Path
+
+'''
+Function to search class ID based on Student ID
+    Parameter : Student ID
+    Return : Class ID
+'''
+
+def search_student(student_id) :
+    # Read data from student data excel and get its sheets name
+    file_path = Path.cwd() / 'data' / 'data_mhs.xlsx'
+    data_frame = pd.read_excel(file_path, sheet_name=None)
+    
+    # Generate class ID from student ID
+    # Ex :181511023 is in class 181501 or 181502
+    angkatan = student_id[0:2]
+    prodi = student_id[4:6]
+    class_id = angkatan+prodi
+
+    # Loop over sheets name and search the student
+    for sheet_name in data_frame.keys() :
+         # check if class id that generated from student id is substring from sheet name (class id)
+         if class_id in sheet_name :
+             # make list of student id from some class
+             studentID_array = map(str, list(set(data_frame[sheet_name]["NIM"])))
+
+             if  student_id in studentID_array:
+                 # student_id is exist in this sheet (class_id)
+                 # return class id
+                 return sheet_name
+    
+    return "000000"
+
+print(search_student('181524007'))
