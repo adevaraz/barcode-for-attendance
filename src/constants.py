42b2bb71c7d8891a7e5bb431ffef03ed2b7d5fec
adevaraz <zaraveda.zv@gmail.com>
2021-03-02 08:47:19 +0700

    fix constants

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,3 @@
-DATA_MATKUL_LOC = "..\data\data_matkul.xlsx"
-DATA_MHS_LOC = "..\data\data_mhs.xlsx"
-PRESENSI_LOC = "..\data\presensi.xlsx"
+DATA_MATKUL_LOC = ".\data\data_matkul.xlsx"
+DATA_MHS_LOC = ".\data\data_mhs.xlsx"
+PRESENSI_LOC = ".\data\presensi.xlsx"
