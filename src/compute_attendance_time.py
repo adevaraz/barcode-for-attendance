f0c90288af471230773632d5111f88a7e075dda6
adevaraz <zaraveda.zv@gmail.com>
2021-03-02 08:31:35 +0700

    try using openpyxl

--- src/compute_attendance_time.py
+++ src/compute_attendance_time.py
@@ -1,6 +1,6 @@
 # import datetime obj
 from datetime import datetime
-import pandas as pd
+import openpyxl
 import constants
 
 '''
@@ -10,28 +10,37 @@ Function to compute whether a student is present, late, or absent
 '''
 def compute_attendance_time(attendance_time, class_id):
     # attendance_time = raw_attendance_time.strftime("%H:%M:%S")
-    day = get_day(attendance_time)
+    # day = get_day(attendance_time)
+    day = "Senin"
 
-    # Read data_matkul.xlsx
-    # In order to read .xlsx file, please install xlrd (for Excel 2003 and Excel 2007)
-    # or openpyxl (for Excel 2007+) first
-    print(constants.DATA_MATKUL_LOC)
-    d_file = pd.read_excel(constants.DATA_MATKUL_LOC, sheet_name=class_id)
-    # TODO: itung selisih terkecil current time ke tiap matkul di current day buat tentuin current matkul
-    print(d_file.iloc[3])
+    if day != "Sabtu" and day != "Minggu":
+        # Read data_matkul.xlsx
+        # In order to read .xlsx file, please install xlrd (for Excel 2003 and Excel 2007)
+        # or openpyxl (for Excel 2007+) first
+        courses_file = openpyxl.load_workbook(constants.DATA_MATKUL_LOC)
+        current_sheet = courses_file[class_id]
+        # TODO: itung selisih terkecil current time ke tiap matkul di current day buat tentuin current matkul
+        print(current_sheet['A1'].value)
+        
+        # raw_start = df.loc[(df["hari"] == day) & (df["Kode_MK"] == 4), ["jam_mulai"]]
+        # raw_end = df.loc[(df["hari"] == day) & (df["Kode_MK"] == 4), ["jam_selesai"]]
 
-    now = datetime.now()
-    start_time = now.replace(hour=14, minute=0, second=0, microsecond=0)
-    end_time = now.replace(hour=15, minute=28, second=0, microsecond=0)
+        # print(raw_start)
+        # print(raw_end)
+        now = datetime.now()
+        start_time = now.replace(hour=16, minute=0, second=0, microsecond=0)
+        end_time = now.replace(hour=16, minute=28, second=0, microsecond=0)
 
-    if attendance_time <= start_time:
-        print(attendance_time)
-        print("Hadir tepat waktu")
-    elif attendance_time >= end_time:
-        print("Telat banged woe dah beres kali")
+        if attendance_time <= start_time:
+            print(attendance_time)
+            print("Hadir tepat waktu")
+        elif attendance_time >= end_time:
+            print("Telat banged woe dah beres kali")
+        else:
+            min_elapsed = minutes_difference(start_time, attendance_time)
+            print("Terlambat " + str(min_elapsed) + " menit")
     else:
-        min_elapsed = minutes_difference(start_time, attendance_time)
-        print("Terlambat " + str(min_elapsed) + " menit")
+        print("Tidak ada jadwal kuliah yang dapat dihadiri.")
 
 '''
 Function to convert date to day name in Indonesian
